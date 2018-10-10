e94d460b75ef947e751aab65675294c2acbf5c42
bdm2505 <bdm1999@yandex.ru>
2018-10-10 14:20:50 +0500

    у вас не корректный тест
    в вашем случае 
    withDouble.sub("ab").sub("cd").get 
    должно быть пустым, а не 42

--- src/test/scala/fintech/homework03/PrefixTreeSpec.scala
+++ src/test/scala/fintech/homework03/PrefixTreeSpec.scala
@@ -8,7 +8,7 @@ class PrefixTreeSpec extends FlatSpec with Matchers {
     val with42: PrefixTree[Char, Int] = tree.put("abcd", 42)
     with42.sub("ab").sub("cd").get should be (42)
 
-    val withDouble: PrefixTree[Char, AnyVal] = tree.put("abcde", 13.0)
+    val withDouble: PrefixTree[Char, AnyVal] = with42.put("abcde", 13.0)
     withDouble.sub("ab").sub("cd").get should be (42)
     withDouble.sub("ab").sub("cde").get should be (13.0)
   }
